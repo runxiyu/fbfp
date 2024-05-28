837fbbfecd3994a625969989ad2f0291216388e5
Runxi Yu <me@runxiyu.org>
2024-05-28 21:05:16 +0800

    Register all routes in make_bp(login_required)
    I don't think this is the best-looking way to do it, and it does mean
    that my indentation is a mess, but it's the sanest way that I can
    imagine.
    
    References: https://todo.sr.ht/~runxiyu/fbfp/1

--- fbfp.py
+++ fbfp.py
@@ -15,41 +15,45 @@ import tempfile
 import shutil
 import datetime
 import zoneinfo
+import functools
 
 import flask
 import werkzeug
 import werkzeug.security
 import werkzeug.middleware.proxy_fix
 
-# NOTE: The following import is *only* for development. When ready to deploy
-# this to production, I should probably create a blueprint construction
-# function as a closure (the constructor should accept a function that does
-# the job of login_required). But that seems to require that I define all of the
-# routes in the blueprint inside the closure, which is annoying for indentation.
-# I guess I'll deal with that later.
-from test_login import login_required
-
 response_t: typing.TypeAlias = typing.Union[werkzeug.Response, flask.Response, str]
+login_required_argument_t: typing.TypeAlias = typing.Callable[[], response_t]
+login_required_t: typing.TypeAlias = typing.Callable[
+    [login_required_argument_t], login_required_argument_t
+]
 
 VERSION = """fbfp v0.1
 
 License: GNU Affero General Public License v3.0 or later
 URL: https://sr.ht/~runxiyu/fbfp"""
 
-bp = flask.Blueprint("fbfp", __name__, url_prefix='/', template_folder="templates")
 
-@bp.route("/", methods=["GET"])
-@login_required
-def index() -> response_t:
-    return flask.Response(flask.render_template("index.html"))
+def make_bp(login_required: login_required_t) -> flask.Blueprint:
+    bp = flask.Blueprint("fbfp", __name__, url_prefix="/", template_folder="templates")
 
-def make_app() -> flask.App:
+    @bp.route("/", methods=["GET"])
+    @login_required
+    def index() -> response_t:
+        return flask.Response(flask.render_template("index.html"))
+
+    return bp
+
+
+def make_app(login_required: login_required_t) -> flask.App:
     app = flask.Flask(__name__)
     app.wsgi_app = werkzeug.middleware.proxy_fix.ProxyFix(  # type: ignore
         app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
     )
-    app.register_blueprint(bp, url_prefix='/')
+    app.register_blueprint(make_bp(login_required), url_prefix="/")
     return app
 
+
 if __name__ == "__main__":
-    make_app().run(port=8080, debug=True)
+    # This is obviously only for development.
+    make_app(login_required=lambda i: i).run(port=8080, debug=True)
--- test_login.py
+++
@@ -1,8 +0,0 @@
-import flask
-import functools
-
-def login_required(f):
-    @functools.wraps(f)
-    def _(*args, **kwargs):
-        return f(*args, **kwargs)
-    return _
