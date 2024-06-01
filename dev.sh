83f12c0b597d57e302b757511c7ae0ef1b7cf68b
Runxi Yu <me@runxiyu.org>
2024-06-01 08:00:00 +0800

    dev.sh: Start and stop mariadb

--- dev.sh
+++ dev.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+set -x
+
+signal_handler () {
+	echo
+	sudo systemctl stop mariadb
+	exit
+}
+
+trap signal_handler INT
+
+sudo systemctl start mariadb
+
 while true
 do
 	./venv/bin/python3 -m flask --app 'fbfp:make_debug_app()' run --debug
