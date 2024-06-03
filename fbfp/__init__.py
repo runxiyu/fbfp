b667c7c45b481ad6bc059b2139062aa86503806e
Runxi Yu <me@runxiyu.org>
2024-06-03 08:00:00 +0800

    Am I extra stupid?

--- fbfp/__init__.py
+++ fbfp/__init__.py
@@ -284,6 +284,7 @@ def make_bp(login_required: login_required_t) -> flask.Blueprint:
                 # FIXME: Is this a race condition or does it violate file locking?
             form_file.save(local_filename)
             os.umask(oldmask)
+            os.chmod(local_filename, "0o644")
 
         else:
             local_filename = None
@@ -396,6 +397,7 @@ def make_bp(login_required: login_required_t) -> flask.Blueprint:
                 local_filename = fd.name
                 form_file.save(local_filename)
             os.umask(oldmask)
+            os.chmod(local_filename, "0o644")
         else:
             local_filename = None
 
