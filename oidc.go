a9468839263694050ca3b1344f5a9c1673af45ac
Runxi Yu <me@runxiyu.org>
2024-08-16 08:00:00 +0800

    Incomplete JWE parsing

--- oidc.go
+++ oidc.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 var openid_configuration struct {
@@ -13,6 +15,14 @@ var openid_configuration struct {
 	TokenEndpointAuthMethodsSupported ([]string) `json:"token_endpoint_auth_methods_supported"`
 	JwksUri                           string     `json:"jwks_uri"`
 	UserinfoEndpoint                  string     `json:"userinfo_endpoint"`
+}
+
+type jose_header_t struct {
+	Typ string `json:"typ"`
+	Cty string `json:"cty"`
+	Alg string `json:"alg"`
+	X5t string `json:"x5t"`
+	Kid string `json:"kid"`
 }
 
 /*
@@ -79,15 +89,48 @@ func handle_oidc(w http.ResponseWriter, req *http.Request) {
 		if returned_error_description == "" {
 			w.Header().Set("Content-Type", "text/plain")
 			w.WriteHeader(400)
-			w.Write([]byte(fmt.Sprintf("Error: The OpenID Connect remote returned an error %s, but did not provide an error_description.\n", returned_error)))
+			w.Write([]byte(fmt.Sprintf(
+				"Error: The OpenID Connect callback returned an error %s, but did not provide an error_description.\n",
+				returned_error,
+			)))
 			return
 		} else {
 			w.Header().Set("Content-Type", "text/plain")
 			w.WriteHeader(400)
-			w.Write([]byte(fmt.Sprintf("Error: The OpenID Connect remote returned an error.\n\n%s\n\n%s\n", returned_error, returned_error_description)))
+			w.Write([]byte(fmt.Sprintf(
+				"Error: The OpenID Connect callback returned an error.\n\n%s\n\n%s\n",
+				returned_error,
+				returned_error_description,
+			)))
 			return
 		}
 	}
+
+	id_token := req.PostFormValue("id_token")
+	if id_token == "" {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(400)
+		w.Write([]byte(fmt.Sprintf("Error: The OpenID Connect callback did not return an error, but no id_token was found.\n")))
+		return
+	}
+
+	id_token_split := strings.Split(id_token, ".")
+	if len(id_token_split) != 3 {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(400)
+		w.Write([]byte(fmt.Sprintf("Error: The JSON Web Token provided in id_token does not contain 3 fields separated by 2 '.'s. While it is technically valid for the signature part to be missing according to section 7.2 of RFC 7519, we do not accept unsigned JWTs. JWEs are not acceptable either.\n")))
+		return
+	}
+	jose_header_base64u, jwt_payload_base64u, jwt_signature_base64u := id_token_split[0], id_token_split[1], id_token_split[2]
+	jose_header_json, err := base64.RawURLEncoding.DecodeString(jose_header_base64u)
+	if err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(400)
+		w.Write([]byte(fmt.Sprintf("Error: Malformed base64url encoding of the JOSE header.\n")))
+		return
+	}
+	var jose_header jose_header_t
+	err = json.Unmarshal(jose_header_json, &jose_header)
 
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(200)
