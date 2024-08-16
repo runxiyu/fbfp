d931e3f34615167c84fae739dcc7e8fd51aeeebb
Runxi Yu <me@runxiyu.org>
2024-08-16 08:00:00 +0800

    Basic scfg

--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
 module git.sr.ht/~runxiyu/fbfp
+
+go 1.22.5
+
+require (
+	git.sr.ht/~emersion/go-scfg v0.0.0-20240128091534-2ae16e782082 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v1.2.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,4 +1,30 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"git.sr.ht/~emersion/go-scfg"
+)
+
+var config struct {
+	Port *int `scfg:"port"`
+}
+
+func e(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
 func main() {
+	f, err := os.Open("fbfp.scfg")
+	e(err)
+
+	err = scfg.NewDecoder(bufio.NewReader(f)).Decode(&config)
+	e(err)
+
+	fmt.Println(*(config.Port))
 }
